d885bfc338cf040d2b476855f3afb5264abcb053
grantspell <spellgrant@gmail.com>
2018-01-04 15:24:45 -0500

    CREATED | "Index" and "Show" routes

--- routes/creatureController.js
+++ routes/creatureController.js
@@ -3,3 +3,25 @@ const router = express.Router()
 
 const { Creature } = require('../db/schema.js')
 
+// Index Route
+router.get('/', async (req, res) => {
+    try {
+        const creatures = await Creature.find({})
+        res.json(creatures)
+    } catch (err) {
+        console.log(err)
+    }
+})
+
+// Show Route
+router.get('/:id', async (req, res) => {
+    try {
+        const creatureId = req.params.id
+        const creature = await Creature.findById(creatureId)
+        res.json(creature)
+    } catch (err) {
+        console.log(err)
+        res.json(err)
+    }
+})
+
